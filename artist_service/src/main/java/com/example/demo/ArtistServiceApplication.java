ddb7b4174170d35f618e4d467fd095a32ed94c33
Gabriela Soruco <soruco.gabi@gmail.com>
2022-09-05 15:59:46 -0300

    swagger simulation

--- artist_service/src/main/java/com/example/demo/ArtistServiceApplication.java
+++ artist_service/src/main/java/com/example/demo/ArtistServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "Artist API", version = "1.0", description = "Artist microservice"))
 public class ArtistServiceApplication {
 
 	public static void main(String[] args) {
