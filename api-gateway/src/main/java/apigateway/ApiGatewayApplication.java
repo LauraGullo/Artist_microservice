272a2d6c9b5ab5f52e6af3de5c4ae8904af4d4f6
Gabriela Soruco <soruco.gabi@gmail.com>
2022-09-08 00:30:05 -0300

    simulation swagger-gateway

--- api-gateway/src/main/java/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,16 @@
 package apigateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springdoc.core.GroupedOpenApi;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +18,22 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Autowired
+	RouteDefinitionLocator locator;
+
+	public List<GroupedOpenApi> apis(){
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId()
+				.matches(".*-service"))
+				.forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("-service", "");
+			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+		});
+		return groups;
 	}
 
 }
